eb5423a8b9d978c0942cf9fdb8b1f631e1619e7d
neural-navigator <iamsatyapati@gmail.com>
2024-07-06 10:56:47 +0530

    signup added

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const Landingpage = () => {
   return (
     <div>
       Landingpage
+
+      <nav>
+        <Link to="signup">Signup</Link>
+        <Link to="login">Login</Link>
+      </nav>
+      <Outlet />
     </div>
   )
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Signup = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setUser(prevUser => ({
+      ...prevUser,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    console.log(user);
+    const response = await axios.post("http://localhost:3002/users", user);
+    console.log(response);
+  }
+
   return (
     <div>
       Signup page
+      <form onSubmit={handleSubmit}>
+        <label>
+          Full Name: <input type="text" id="name" name="name" value={user.name} onChange={handleChange} required/>
+        </label>
+        <label>
+          Email: <input type="email" id="email" name="email" value={user.email} onChange={handleChange} required/>
+        </label>
+        <label>
+          Password: <input type="password" id="password" name="password" value={user.password} onChange={handleChange} required/>
+        </label>
+        <button type="submit">Signup</button>
+      </form>
     </div>
   )
 }
