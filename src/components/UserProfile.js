1d1ccc42afa7305a428db4369b3e39056ccf70ec
neural-navigator <iamsatyapati@gmail.com>
2024-07-14 02:17:55 +0530

    user profile shown

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from './AuthProvider';
 import { Link, Outlet } from 'react-router-dom';
+import axios from 'axios';
 
 const UserProfile = () => {
     const auth = useAuth();
+    const [user, setUser] = useState(null);
+
+    const headers = {
+      headers : {
+        Authorization: `Bearer ${auth.user.sessionToken}`
+      }
+    }
+
+    useEffect (() => {
+      const fetchUserProfile = async() => {
+        try {
+          const response = await axios.get("http://localhost:4001/api/v1/get-user-profile", headers);
+          console.log(response.data.userProfile);
+          setUser(response.data.userProfile);
+        } catch(error) {
+          console.log(`Error :: ${error}`);
+          setUser(null);
+        }
+        
+      }
+      fetchUserProfile();
+    }, [auth.user.sessionToken])
+
+    
   return (
     <div>
-      Hello, {auth.user.username}
+      <h1>{user.name}</h1>
+      <h2>{user.email}</h2>
+      <h2>{user.createdAt}</h2>
       <nav>
         <Link to="/edit-profile">Edit Profile</Link>
       </nav>
