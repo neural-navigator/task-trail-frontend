f9bd1866435578231cfed8ec865ee4481e9b21c5
neural-navigator <iamsatyapati@gmail.com>
2024-07-27 13:51:02 +0530

    login mui intergrated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthProvider';
-
+import { useState } from 'react';
 
 const Login = () => {
 
@@ -19,6 +33,8 @@ const Login = () => {
       password: ''
     })
   }
+
+  const defaultTheme = createTheme();
 
   const handleChange = (e) => {
     const {name, value} = e.target;
@@ -42,20 +58,76 @@ const Login = () => {
   }
 
   return (
-    <div>
-      login page
-      <form onSubmit={handleLogin}>
-      <label>
-        User Email:
-          <input type="text" name="email" id="email" value={user.email} onChange={handleChange}/>
-      </label>
-      <label>
-        User Password:
-          <input type="password" name="password" id="password" value={user.password} onChange={handleChange}/>
-      </label>
-      <button type="submit">Login</button>
-      </form>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+      
+      <Box component="form" onSubmit={handleLogin} noValidate sx={{mt:1}} >
+        <TextField
+          Margin="normal"
+          required
+          fullWidth
+          id="email"
+          name="email"
+          label="User Email"
+          autoComplete="email"
+          autoFocus
+          value={user.email}
+          onChange={handleChange} />
+        
+        <TextField
+          marhin="normal"
+          required
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          autoComplete="current-password"
+          value={user.password}
+          onChange={handleChange} />
+        <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+
+      </Box>
+      </Box>
+      </Container>
+      </ThemeProvider>
   )
 }
 
